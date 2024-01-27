0157edeb66090ab43afe6ad0fddc76b8b8f19ad0
hahind <150461471+hahind@users.noreply.github.com>
2024-01-27 20:23:16 +0100

    Hp bar interaction

---
+++ GameJam/Assets/Card2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Cardeffects card2;
+    public Player player;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKeyDown("space")){
+        //player.currenthp = 50;
+    }
+    
+    }
+}
