1c60beb71c380fa3a76a609ca4215ae7ad93a83d
Inderjot01 <Inderjots82@gmail.com>
2024-01-27 22:58:37 +0100

    Inder_mess_fix

--- GameJam/Assets/Scripts/Card1.cs
+++ GameJam/Assets/Scripts/Card1.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class Card1 : MonoBehaviour
 {
     public Cardeffects cardeffects;
+
     // Update is called once per frame
     void Start()
     {
         
     }
+
     void Update()
     {
-        if(Input.GetKeyDown("space")){
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Call a method to handle the destruction
             cardeffects.LoseHp(20);
+
+            // Destroy the object when the mouse button is clicked
+            Destroy(gameObject);
         }
     }
 }
+
